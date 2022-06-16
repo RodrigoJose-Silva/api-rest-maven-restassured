b94e9c7f4aed0ad4fde833836394377b195ad4b7
Rodrigo Jose <digo.1903@hotmail.com>
2022-06-15 23:51:33 -0300

    add testes com Hamcrest e testes de verifação de conteudo do Body

--- src/test/java/testRest/OlaMundoTest.java
+++ src/test/java/testRest/OlaMundoTest.java
@@ -1,10 +1,12 @@
 package testRest;
 
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class OlaMundoTest {
 
@@ -15,10 +17,26 @@ public class OlaMundoTest {
         String baseUrl = "http://restapi.wcaquino.me/";
 
         given()// Pré condições
+            .contentType(ContentType.JSON)
+        .when() // Ação do método
+            .get(baseUrl + "ola")
+        .then() // Verificações - Assertivas
+            .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Teste com a assertivas do Hamcrest")
+    public void conhendoMatchersHamcrest () {
+        String baseUrl = "http://restapi.wcaquino.me/";
+
+        given()// Pré condições
                 .contentType(ContentType.JSON)
         .when() // Ação do método
                 .get(baseUrl + "ola")
         .then() // Verificações - Assertivas
-                .assertThat().statusCode(200);
+                .statusCode(200)
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"))
+                .body(is(not(nullValue())));
     }
 }
---
+++ src/test/java/testRest/UserJsonTest.java
@@ -0,0 +1,50 @@
+package testRest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class UserJsonTest {
+
+    @Test
+    @DisplayName("Deve verificar primeiro item")
+    public void primeiroItem () {
+
+        String baseUrl = "http://restapi.wcaquino.me/";
+
+        given()
+        .when()
+                .get(baseUrl + "users/1")
+        .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("name", containsString("João"))
+                .body("age", greaterThan(18));
+    }
+
+    @Test
+    @DisplayName("Deve verificar primeiro nivel de outras formas")
+    public void verificandoPrimeiroNivelDeOutraForma () {
+
+        String baseUrl = "http://restapi.wcaquino.me/";
+
+        Response response = request(Method.GET,baseUrl + "users/1");
+
+        //path
+        Assertions.assertEquals(new Integer(1), response.path("id"));
+
+        //jsonpath
+        JsonPath jpath = new JsonPath(response.asString());
+        Assertions.assertEquals(1, jpath.getInt("id"));
+
+        //from
+        int id = JsonPath.from(response.asString()).getInt("id");
+        Assertions.assertEquals(1, id);
+    }
+}
