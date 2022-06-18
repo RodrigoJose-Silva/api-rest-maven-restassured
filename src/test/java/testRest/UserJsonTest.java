1397964b0b5d4e8857a20c1e5206b559168e4b89
Rodrigo Jose <digo.1903@hotmail.com>
2022-06-18 14:04:11 -0300

    Add caso de teste de "verificações avançadas"

--- src/test/java/testRest/UserJsonTest.java
+++ src/test/java/testRest/UserJsonTest.java
@@ -109,4 +109,28 @@ public class UserJsonTest {
                 .body("salary", contains(1234.5677f, 2500, null)) // validando os valores do atributo "salary" na lista raiz
                 ;
     }
+
+    @Test
+    @DisplayName("Devo fazer verificações avançadas")
+    public void verificacoesAvancadas () {
+
+        given()
+        .when()
+                .get(baseUrl + "users")
+        .then()
+                .statusCode(200)
+                .body("$", hasSize(3)) // validando a qtde de objetos na lista
+                .body("age.findAll{it <= 25}.size()", is(2) ) //verifica na lista qtos valores no atributo "age" são menores ou = a 25
+                .body("age.findAll{it >20 && it <=25}.size()", is(1)) //verifica na lista qtos valores no atributo "age" dentro da faixa informada
+                .body("findAll{it.age >20 && it.age <= 25}.name", hasItem("Maria Joaquina")) //verifica na lista o valor no atributo "name" dentro da faixa informada do atributo "age"
+                .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina")) //verifica na lista o primeiro valor no atributo "name" dentro da faixa informada do atributo "age"
+                .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia")) //verifica na lista no atributo "name", se possui algum valor que contém a letra informada
+                .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina")) //verifica na lista no atributo "name", se possui algum com mais de 10 caracteres
+                .body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA")) // verifica o valor do atributo "name" e passa para caixal alta
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA")) //verifica em toda a lista no atributo "name", iniciado com "Maria' convertendo a validação para caixa alta
+                .body("age.collect{it * 2}", hasItems(60, 50, 40)) // pega os valores do atributo "age" multiplica por 2 e valida na sequência
+                .body("id.max()", is(3)) // verifica o maior valor do atributo "id" na lista
+                .body("salary.min()", is(1234.5677F)) // verifica o menor valor do atributo "salary" da lista
+                .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678F, 0.001))); // verifica a soma dos valores diferente de NULL do atributo "salary"
+    }
 }
