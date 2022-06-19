50bceba25c2a690baa6da5ae242d489f9f015516
Rodrigo Jose <digo.1903@hotmail.com>
2022-06-19 19:44:39 -0300

    Add o package swapiRest, a Class authTest testando uma nova API "https://api.openweathermap.org/data/2.5/weather"".

--- src/test/java/swapiRest/authTest.java
+++ src/test/java/swapiRest/authTest.java
@@ -29,4 +29,26 @@ public class authTest {
                 .body("name", is("Luke Skywalker"))
         ;
     }
+
+    @Test
+    @DisplayName("Deve obter clima")
+    public void obterClima () {
+        given()
+                .log().all()
+                .queryParam("q", "Fortaleza,BR")
+                .queryParam("appid", "b47c51cfc4cbd6cd97dc0a3aa87e69f1")
+                .queryParam("units", "metric")
+        .when()
+                .post("https://api.openweathermap.org/data/2.5/weather")
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Fortaleza"))
+                .body("coord.lon", is(-38.5247F))
+                .body("main.temp", is(27.07F))
+                .body("main.temp", greaterThan(25F))  // verifica que o valor do atributo "temp" é maior que 25
+        ;
+    }
 }
+
+//b47c51cfc4cbd6cd97dc0a3aa87e69f1
